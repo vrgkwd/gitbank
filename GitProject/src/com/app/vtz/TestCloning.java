a02eaa639284d324b35fd84c053a719c4c71ac4b
User <User@User-PC>
2021-04-21 11:49:11 +0530

    DEV1 add sysout for logging purpose

--- GitProject/src/com/app/vtz/TestCloning.java
+++ GitProject/src/com/app/vtz/TestCloning.java
@@ -4,6 +4,6 @@ public class TestCloning {
 
 	public static void main(String[] args) {
 		System.out.println("welcome to clone program");
-		System.out.println("developer 1 ::work properly");
+		System.out.println("developer 1  ::work properly");
 	}
 }
