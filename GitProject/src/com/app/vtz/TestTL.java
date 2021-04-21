5c8e5024ca0daeaefd50ac92764130343c653f87
User <User@User-PC>
2021-04-21 15:44:20 +0530

    DEV1 is added

--- GitProject/src/com/app/vtz/TestTL.java
+++ GitProject/src/com/app/vtz/TestTL.java
@@ -4,6 +4,6 @@ public class TestTL {
 
 	public static void main(String[] args) {
 		System.out.println("this file is craeted by Team Lead");
-		System.out.println("Hello all, HOW ARE YOU");
+		System.out.println("Hello all, HOW ARE YOU--");
 	}
 }
