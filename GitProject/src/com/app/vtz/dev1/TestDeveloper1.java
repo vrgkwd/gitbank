652d2b74c4456c0530945d9c26c4ffeebea972ff
User <User@User-PC>
2021-04-21 15:27:42 +0530

    TL is added for dev1

--- GitProject/src/com/app/vtz/dev1/TestDeveloper1.java
+++ GitProject/src/com/app/vtz/dev1/TestDeveloper1.java
@@ -3,7 +3,7 @@ package com.app.vtz.dev1;
 public class TestDeveloper1 {
 	
 	public static void main(String[] args) {
-		System.out.println("FROM TEST DEVEOPER _1--->added for  Teamleader");
+		System.out.println("FROM TEST DEVEOPER _1--->added for  Dev1");
 		
 	}
 }
