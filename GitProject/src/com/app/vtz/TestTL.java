df269f56dd1d567153866988a075a2587cf7d7e0
User <User@User-PC>
2021-04-21 17:05:33 +0530

    TL add the tag but not checkOUT

--- GitProject/src/com/app/vtz/TestTL.java
+++ GitProject/src/com/app/vtz/TestTL.java
@@ -5,5 +5,6 @@ public class TestTL {
 	public static void main(String[] args) {
 		System.out.println("this file is craeted by Team Lead");
 		System.out.println("Hello all, HOW ARE YOU--");
+		System.out.println("---after craeting the TAG---");
 	}
 }
